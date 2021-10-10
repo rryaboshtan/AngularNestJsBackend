335ea77eca54489a8b4156cebd03e26c7713bfce
rryaboshtan <rrvworkroman@gmail.com>
2021-10-10 16:26:28 +0300

    jwt install

--- dist/app.module.js
+++ dist/app.module.js
@@ -12,11 +12,12 @@ const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const users_module_1 = require("./users/users.module");
 const auth_module_1 = require("./auth/auth.module");
+const config_1 = require("@nestjs/config");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [users_module_1.UsersModule, auth_module_1.AuthModule],
+        imports: [users_module_1.UsersModule, auth_module_1.AuthModule, config_1.ConfigModule.forRoot()],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
