811b469465eb121b2b16af16aef05c25028d32b2
rryaboshtan <rrvworkroman@gmail.com>
2021-10-12 01:58:39 +0300

    Add configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret', //configService.get<string>('JWT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
