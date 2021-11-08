b4bd76875b0277c27ac350d53973ec27270bcf51
rryaboshtan <rrvworkroman@gmail.com>
2021-11-08 22:49:13 +0200

    add and getMenu

--- src/modules/admin-menu/menus/menu.service.ts
+++ src/modules/admin-menu/menus/menu.service.ts
@@ -1,25 +1,65 @@
-import { Injectable } from "@nestjs/common";
-import { MenuNode, PatchMenuNode } from "./menu-node";
+import { Injectable } from '@nestjs/common';
+import { MenuNode, PatchMenuNode, ROOT_MENU_NODE_ID } from './menu-node';
 
 @Injectable()
 export class MenuService {
+  private nodes: { [id: string]: MenuNode } = {};
+  private patches: PatchMenuNode[] = [];
+  /**
+   *
+   */
+  getMenu(): MenuNode[] {
+    const src = Object.values(this.nodes);
 
-    /**
-     * 
-     */
-    getMenu(): MenuNode[] {
-        return [];
-    }
+    return this.getMenuForNode(ROOT_MENU_NODE_ID, src);
+  }
 
-    add(...nodes: MenuNode[]): void {
-         
-    }
+  getMenuForNode(id: string, src: MenuNode[]): MenuNode[] {
+    return src
+      .filter((node) => node.parentId === id)
+      .map(({ href, ...node }) => {
+        const children = this.getMenuForNode(node.id, src);
 
-    patch(...nodes: PatchMenuNode[]): void {
+        //if this node has children, then it will not has href, because it is a page node, not the menu item
+        if (children.length > 0) {
+          return {
+            ...node,
+            children,
+          };
+        }
 
-    }
+        return {
+          ...node,
+          href,
+          //we shall delete node, if it not has the meaningful href
+          removed: !href,
+          children: [],
+        };
+      })
+      .filter((node) => !node.removed);
+  }
 
-    remove(...ids: string[]): void {
-        
-    }
+  add(...nodes: MenuNode[]): void {
+    nodes.forEach((node) => {
+      const sanitizedChildren =
+        node.children?.map((child) => ({
+          ...child,
+          parentId: node.id,
+        })) || [];
+
+      const { children, ...sanitizedNode } = node;
+      this.add(...sanitizedChildren);
+
+      this.nodes[node.id] = sanitizedNode;
+    });
+  }
+
+  patch(...patches: PatchMenuNode[]): void {
+    this.patches = [
+      ...this.patches,
+      ...patches,
+    ]
+  }
+
+  remove(...ids: string[]): void {}
 }
